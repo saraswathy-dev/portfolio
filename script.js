ea8dfa32f774d58c76514a72c00e637baa3a145e
saraswathy <saraswathychandra88@gmail.com>
2023-05-05 14:32:36 +0200

    first commit with all pages done

---
+++ script.js
@@ -0,0 +1,13 @@
+// about tabs
+const tabContainer=document.querySelector(".about-tabs");
+aboutSection=document.querySelector(".about-section");
+
+tabContainer.addEventListener("click",(e)=>{
+if(e.target.classList.contains("tab-items") && !e.target.classList.contains("active")){
+ tabContainer.querySelector(".active").classList.remove("active");
+ e.target.classList.add("active");
+ const target=e.target.getAttribute("data-target");
+ aboutSection.querySelector(".tab-content.active").classList.remove("active");
+ aboutSection.querySelector(target).classList.add("active");
+}
+})
